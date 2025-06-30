ed728592227d0408131a346652e9f479c035491a
Corentin Tournier <corentin.tournier@ikigaifreelance.com>
2025-06-30 12:34:19 +0200

    responsive Home and screenshots

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,19 +13,23 @@ export function HomePage() {
   const countries: Country[] = data.countries;
 
   return (
-    <section className="w-full flex p-12 gap-10">
-      <div className="w-1/2 p-4 pr-30">        
+    <section className="w-full flex flex-col lg:flex-row p-6 lg:p-12 gap-10">
+      <div className="w-full lg:w-1/2 p-4">
         <h2 className="text-2xl font-bold mb-8">Ajouter un pays</h2>
         <AddCountryForm updateList={refetch} />
       </div>
-      <div className="w-1/2 p-4">
+      <div className="w-full lg:w-1/2 p-4">
         <h2 className="text-2xl font-bold mb-8">Liste des pays</h2>
-        <ul className="grid grid-cols-2 gap-4 overflow-auto max-h-96">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4 overflow-auto max-h-96">
           {countries.map((country) => (
-            <a key={country.id} className="flex flex-col items-center justify-center border border-gray-300 p-4 rounded" href={`/country/${country.code}`}>
+            <a
+              key={country.id}
+              href={`/country/${country.code}`}
+              className="flex flex-col items-center justify-center border border-gray-300 p-4 rounded"
+            >
               <span className="text-2xl">{country.emoji}</span>
               <h3 className="text-lg font-semibold mb-2">{country.name}</h3>
-              {country.continent ? <p className="text-sm">{country.continent?.name}</p> : null}
+              {country.continent ? <p className="text-sm">{country.continent.name}</p> : null}
             </a>
           ))}
         </ul>
